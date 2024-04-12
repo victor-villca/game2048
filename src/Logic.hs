8ddcf4e2160f2f78a604f4a02f2b104919694052
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2024-04-11 22:27:21 -0400

    Avance random

--- src/Logic.hs
+++ src/Logic.hs
@@ -9,5 +9,29 @@ createEmptyBoard :: Int -> Board
 createEmptyBoard n = array indexRange $ zip (range indexRange) (cycle [Empty])
     where indexRange = ((0, 0), (n - 1, n - 1))
 
+getEmptyCells :: Board -> [(Int, Int)]
+getEmptyCells board = filter (\coord -> board ! coord == Empty) (indices board)
+
+randomNumber :: Int -> Int -> IO Int
+randomNumber min max = randomRIO (min, max)
+
+getRandomCell :: [(Int, Int)] -> IO (Int, Int)
+getRandomCell [] = return (-1, -1)
+getRandomCell list = do
+    randomIndex <- randomNumber 0 (length list - 1)
+    return (list !! randomIndex)
+
+generateNumber :: IO Int
+generateNumber = do
+    rand <- randomNumber 1 10
+    if rand == 10
+        then return 4
+        else return 2
+
+generateRandomCell :: Board -> IO Board
+generateRandomCell board = do
+    number <- generateNumber
+    cell <- getRandomCell (getEmptyCells board)
+    return (board // [(cell, Ocuppied number)])
 
 transformGame _ game = game
