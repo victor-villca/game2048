c4124f9b51af69805ec75a3666b2bfb876ce21c6
MayerliSantander <mayerlisantander0@gmail.com>
2024-04-12 20:59:39 -0400

    Add random tile generation after each move and refactor draw cell

--- src/Logic.hs
+++ src/Logic.hs
@@ -7,8 +7,6 @@ import Graphics.Gloss.Interface.Pure.Game
 import System.Random
 import Data.List (transpose)
 
-
-
 -- Función para crear un nuevo tablero vacío
 createEmptyBoard :: Int -> Board
 createEmptyBoard a = array indexRange $ zip (range indexRange) (cycle [Empty])
@@ -21,6 +19,9 @@ getEmptyCells board = filter (\coord -> board ! coord == Empty) (indices board)
 randomNumber :: Int -> Int -> Int
 randomNumber a b = unsafeLocalState (randomRIO (a, b))
 
+randomNumber1 :: Int -> Int -> Int
+randomNumber1 a b = unsafeLocalState (randomRIO (a, b))
+
 getRandomCell :: [(Int, Int)] -> (Int, Int)
 getRandomCell [] = (-1, -1)
 getRandomCell list = list !! randomNumber 0 (length list - 1)
@@ -32,15 +33,24 @@ getRandomNumber
   where
     a = randomNumber 0 9
 
-generateRandomCell :: Board -> Board
-generateRandomCell board = board // [(getRandomCell (getEmptyCells board), Ocuppied getRandomNumber)]
+getRandomNumber1 :: Int
+getRandomNumber1 
+  | a == 2 = 4
+  | otherwise = 2
+  where
+    a = randomNumber1 1 10
 
+generateRandomCell :: Board -> Board
+generateRandomCell board = 
+  | getEmptyCells board /= [] = board // [(getRandomCell (getEmptyCells board), Ocuppied getRandomNumber)]
+
+generateRandomCell1 :: Board -> Board
+generateRandomCell1 board = board // [(getRandomCell (getEmptyCells board), Ocuppied getRandomNumber1)]
 genBoard :: Int -> Board
 genBoard x = generateRandomCell (createEmptyBoard x)
 
 initialBoard :: Int -> Board
-initialBoard x = generateRandomCell (genBoard x)
-
+initialBoard n = generateRandomCell1 (genBoard n)
 
 -- Shifts all the non empty cells in a row to the left
 -- Empty cells are moved to the right side of the row
@@ -50,18 +60,18 @@ shiftRow row = filtered ++ padding
     filtered = filter (/= Empty) row
     padding = replicate (length row - length filtered) Empty
 
-
 -- Performs a move in the game in a given direction
 -- The game board is updated based on it
 performMove :: Direction -> Game -> Game
 performMove direction game = game { gameBoard = newBoard }
   where
     board = gameBoard game
-    newBoard = case direction of
+    movedBoard = case direction of
       TopMov -> moveBoard  TopMov board
       DownMov -> moveBoard  DownMov board
       LeftMov -> moveBoard LeftMov board
       RightMov -> moveBoard  RightMov board
+    newBoard = generateRandomCell movedBoard
 
 -- Moves all cells in the board towards the specified direction
 -- Cells are shifted row by row or column by column, depending on the direction 
@@ -71,12 +81,11 @@ moveBoard direction board = array ((0, 0), (n - 1, n - 1)) newCells
   where
     rows = [[board ! (i, j) | j <- [0..n-1]] | i <- [0..n-1]] --List of rows where each row is  a list of the cells of the board
     newRows = case direction of
-      TopMov -> map (reverse . shiftRow . reverse) rows
-      DownMov -> map shiftRow rows
-      LeftMov -> transpose $ map shiftRow $ transpose rows
-      RightMov -> transpose $ map (reverse . shiftRow . reverse) $ transpose rows
+      TopMov -> transpose $ map shiftRow $ transpose rows
+      DownMov -> transpose $ map (reverse . shiftRow . reverse) $ transpose rows
+      LeftMov -> map shiftRow rows
+      RightMov -> map (reverse . shiftRow . reverse) rows
     newCells = [((i, j), newRows !! i !! j) | i <- [0..n-1], j <- [0..n-1]] --We use !! cause we are indexing from a list, and we do it twice to ge the row and then the column
-
 
 transformGame :: Event -> Game -> Game
 transformGame (EventKey (SpecialKey KeyUp) Down _ _) game = performMove TopMov game
--- src/Rendering.hs
+++ src/Rendering.hs
@@ -9,7 +9,6 @@ import Utils
 boardGridColor :: Color
 boardGridColor = makeColorI 0 0 0 255 
 
-
 -- combines all the lines of the board into a single picture
 -- where drawlines generate a pair of lines for each valui in the list
 boardGrid :: Picture
@@ -22,11 +21,10 @@ boardGrid = pictures $ concatMap drawLines [0.0 .. fromIntegral n]
 
 drawCell :: (Int, Int) -> Cell -> Picture
 drawCell _ Empty = blank
-drawCell (x, y) (Ocuppied value) = pictures [ coloredRectangle, translatedText ]
+drawCell (y, x) (Ocuppied value) = pictures [ coloredRectangle, translatedText ]
   where
-    coloredRectangle = translate (fromIntegral x * cellWidth + cellWidth / 2) (fromIntegral y * cellHeight + cellHeight / 2) $ color red $ rectangleSolid cellWidth cellHeight
-    translatedText = translate (fromIntegral x * cellWidth + cellWidth / 2 -20 ) (fromIntegral y * cellHeight + cellHeight / 2 -20) $ scale 0.5 0.5 $ boldText 1.4 $ color white $ text (show value)
-
+    coloredRectangle = translate (fromIntegral x * cellWidth + cellWidth / 2) (fromIntegral (n-y) * cellHeight - cellHeight / 2) $ color red $ rectangleSolid cellWidth cellHeight
+    translatedText = translate (fromIntegral x * cellWidth + cellWidth / 2 -20 ) (fromIntegral (n-y) * cellHeight - cellHeight / 2 -20) $ scale 0.5 0.5 $ boldText 1.4 $ color white $ text (show value)
 
 -- Iteratates throught the board to draw each cell and combines it with the board grid 
 -- as one single picture
